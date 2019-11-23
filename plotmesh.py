be9bdbfe2b1aff2f557d5479122f2db896e84fa6
Lawrence Cheung <lcheung@sandia.gov>
2019-11-22 19:33:22 -0700

    Added data probe plotting capability

--- plotmesh.py
+++ plotmesh.py
@@ -6,6 +6,9 @@ import sys
 import matplotlib.pyplot    as plt
 from matplotlib.collections import PatchCollection
 from matplotlib.patches     import Rectangle
+import argparse
+
+# Handle arguments
 
 #yamlfile='abl_preprocess.yaml'
 if len(sys.argv)>1:
@@ -22,32 +25,31 @@ with open(yamlfile) as stream:
         print(exc)
 
 
-## Load data from the yamlfile
-# -- mesh extents --
-meshvertices     = yamldata['nalu_abl_mesh']['vertices']
-x0               = meshvertices[0]
-x1               = meshvertices[1]
-# -- mesh dimensions --
-meshdimensions   = yamldata['nalu_abl_mesh']['mesh_dimensions']
 
-if 'mesh_local_refinement' in yamldata['nalu_preprocess']:
-    # -- turbine locations --
-    turbineXY  = yamldata['nalu_preprocess']['mesh_local_refinement']['turbine_locations']
-    turbineDin = yamldata['nalu_preprocess']['mesh_local_refinement']['turbine_diameters']
-    turbineHHin= yamldata['nalu_preprocess']['mesh_local_refinement']['turbine_heights']
-    # make sure turbineD is the right size
-    if isinstance(turbineDin, list):  turbineD = turbineDin
-    else:                             turbineD = [turbineDin]*len(turbineXY)
-    # make sure turbineHH is the right size
-    if isinstance(turbineHHin, list):  turbineHH = turbineHHin
-    else:                              turbineHH = [turbineHHin]*len(turbineXY)
+if 'nalu_preprocess' in yamldata:
+    has_preprocess = True
+else:
+    has_preprocess = False
 
-    # -- Wind direction --
-    orienttype = yamldata['nalu_preprocess']['mesh_local_refinement']['orientation']['type']
-    winddir    = yamldata['nalu_preprocess']['mesh_local_refinement']['orientation']['wind_direction']
+if has_preprocess:
+    if 'mesh_local_refinement' in yamldata['nalu_preprocess']:
+        # -- turbine locations --
+        turbineXY  = yamldata['nalu_preprocess']['mesh_local_refinement']['turbine_locations']
+        turbineDin = yamldata['nalu_preprocess']['mesh_local_refinement']['turbine_diameters']
+        turbineHHin= yamldata['nalu_preprocess']['mesh_local_refinement']['turbine_heights']
+        # make sure turbineD is the right size
+        if isinstance(turbineDin, list):  turbineD = turbineDin
+        else:                             turbineD = [turbineDin]*len(turbineXY)
+        # make sure turbineHH is the right size
+        if isinstance(turbineHHin, list):  turbineHH = turbineHHin
+        else:                              turbineHH = [turbineHHin]*len(turbineXY)
 
-    # -- refinement boxes --
-    refineboxes      = yamldata['nalu_preprocess']['mesh_local_refinement']['refinement_levels']
+        # -- Wind direction --
+        orienttype = yamldata['nalu_preprocess']['mesh_local_refinement']['orientation']['type']
+        winddir    = yamldata['nalu_preprocess']['mesh_local_refinement']['orientation']['wind_direction']
+
+        # -- refinement boxes --
+        refineboxes      = yamldata['nalu_preprocess']['mesh_local_refinement']['refinement_levels']
 
 #print refineboxes
 
@@ -122,7 +124,7 @@ def plotXYpoints(XYpoints):
 
 # Plot the base mesh
 def plotbasemeshXY(p0, p1, meshdimensions):
-    rect=Rectangle((p0[0], p0[1]), p1[0]-p0[0], p1[1]-p0[0])
+    rect=Rectangle((p0[0], p0[1]), p1[0]-p0[0], p1[1]-p0[1])
     currentAxis = plt.gca()
     currentAxis.add_patch(rect)
     return
@@ -170,30 +172,89 @@ def plotwinddirarrow(p0, p1, winddir):
     plt.arrow(center[0], center[1], dx, dy, width=0.05*alength)
     return
 
+def plotLineOfSite(tip, tail, npoints):
+    # construct the line of points
+    dx=(np.array(tip)-np.array(tail))/(npoints-1)
+    x=[]
+    y=[]
+    z=[]
+    for i in range(npoints):
+        pt=np.array(tail)+float(i)*dx
+        x.append(pt[0])
+        y.append(pt[1])
+        z.append(pt[2])
+    plt.plot(x, y, '.', color='k')
+    return
+
+def plotSamplePlane(corner, edge1, edge2, edge1N, edge2N, offsetdir=[], offsetspacings=[]):
+    # construct the line of points
+    dx=(np.array(edge1))/(edge1N-1)
+    dy=(np.array(edge2))/(edge2N-1)
+    x=[]
+    y=[]
+    z=[]
+    for i in range(edge1N):
+        for j in range(edge2N):
+            pt=np.array(corner) + float(i)*dx + float(j)*dy
+            x.append(pt[0])
+            y.append(pt[1])
+            z.append(pt[2])
+    if ((len(offsetdir)>0) and (len(offsetspacings)>0)):
+        for s in offsetspacings:
+            plt.plot(np.array(x)+s*offsetdir[0], np.array(y)+s*offsetdir[1], '.', color='k')
+    else:
+        plt.plot(x, y, '.', color='k')
+    return
+
+def readFASTfile(FASTfile, keyword):
+    # go through the file line-by-line
+    with open(FASTfile) as fp:
+        line=fp.readline()
+        while line:
+            linesplit=line.strip().split()
+            if linesplit[1]==keyword: 
+                return linesplit[0]
+            #print line
+            line=fp.readline()
+    return
+
+
 # Initialize and plot the base mesh
 # ---------------------------------
 fig, ax = plt.subplots(1)
-plotbasemeshXY(x0, x1, meshdimensions)
+
+## Load data from the yamlfile
+if 'nalu_abl_mesh' in yamldata:
+    # -- mesh extents --
+    meshvertices     = yamldata['nalu_abl_mesh']['vertices']
+    x0               = meshvertices[0]
+    x1               = meshvertices[1]
+    
+    # -- mesh dimensions --
+    meshdimensions   = yamldata['nalu_abl_mesh']['mesh_dimensions']
+    plotbasemeshXY(x0, x1, meshdimensions)
+
 
 # Plot the local mesh refinement
 # ---------------------------------
-if 'mesh_local_refinement' in yamldata['nalu_preprocess']:
-    # Plot the refinement boxes
-    refinecolors=['c','y','g','r']
-    for iturb, turb in enumerate(turbineXY):
-        for ibox, box in enumerate(refineboxes):
-            boxXY = getRefineBoxXY(turb, turbineD[iturb], box, winddir)
-            plotRefineBox(boxXY, refinecolors[ibox])
+if has_preprocess:
+    if 'mesh_local_refinement' in yamldata['nalu_preprocess']:
+        # Plot the refinement boxes
+        refinecolors=['c','y','g','r']
+        for iturb, turb in enumerate(turbineXY):
+            for ibox, box in enumerate(refineboxes):
+                boxXY = getRefineBoxXY(turb, turbineD[iturb], box, winddir)
+                plotRefineBox(boxXY, refinecolors[ibox])
 
 
-    # Plot the turbines
-    for iturb, turb in enumerate(turbineXY):
-        turbpts=getTurbXYPoints(turb, turbineD[iturb], winddir)
-        plotXYpoints(turbpts)
+        # Plot the turbines
+        for iturb, turb in enumerate(turbineXY):
+            turbpts=getTurbXYPoints(turb, turbineD[iturb], winddir)
+            plotXYpoints(turbpts)
 
-    plotwinddirarrow(x0, x1, winddir)
-else:
-    print "No local mesh refinement"
+        plotwinddirarrow(x0, x1, winddir)
+    else:
+        print "No local mesh refinement"
 
 
 # Plot the mesh slices
@@ -218,6 +279,65 @@ if 'slice_mesh' in yamldata:
 else:
     print "No slicemesh specification"
     
+
+# Plot everything in realms
+# ---------------------------------
+if 'realms' in yamldata:
+
+    # Plot the turbines
+    if 'actuator' in yamldata['realms'][0]:
+        nturbs = yamldata['realms'][0]['actuator']['n_turbines_glob']
+        # Loop through all of the turbines
+        for iturb in range(nturbs):
+            turbxy= yamldata['realms'][0]['actuator']['Turbine'+repr(iturb)]['turbine_hub_pos']
+            fastfile= yamldata['realms'][0]['actuator']['Turbine'+repr(iturb)]['fast_input_filename']
+            # get the fast information
+            #print(turbxy, fastfile)
+            EDFile=readFASTfile(fastfile, 'EDFile').strip('\"')
+            TipRad=float(readFASTfile(EDFile, 'TipRad'))
+            NacYaw=float(readFASTfile(EDFile, 'NacYaw'))
+            print(EDFile, TipRad, NacYaw)
+            turbpts=getTurbXYPoints(turbxy, 2*TipRad, 270-NacYaw)
+            plotXYpoints(turbpts)
+            
+    else:
+        print("No actuator line turbines to plot")
+
+    # Plot the data probes
+    if 'data_probes' in yamldata['realms'][0]:
+        specdata=yamldata['realms'][0]['data_probes']['specifications']
+        for spec in specdata:
+            # Plot the line-of-site vectors
+            if 'line_of_site_specifications' in spec:
+                alllos=spec['line_of_site_specifications']
+                for los in alllos:
+                    Npoints = los['number_of_points']
+                    tip     = los['tip_coordinates']
+                    tail    = los['tail_coordinates']
+                    plotLineOfSite(tip, tail, Npoints)
+            # Plot the line-of-site vectors
+            if 'plane_specifications' in spec:
+                allplanes=spec['plane_specifications']
+                for plane in allplanes:
+                    corner  = plane['corner_coordinates']
+                    edge1   = plane['edge1_vector']
+                    edge2   = plane['edge2_vector']
+                    edge1N  = plane['edge1_numPoints']
+                    edge2N  = plane['edge2_numPoints']
+                    if 'offset_vector' in plane:
+                        offset_vector   = plane['offset_vector']
+                    else:
+                        offset_vector=[]
+                    if 'offset_spacings' in plane:
+                        offset_spacings = plane['offset_spacings']
+                    else:
+                        offset_spacings=[]
+                    plotSamplePlane(corner, edge1, edge2, edge1N, edge2N, 
+                                    offsetdir=offset_vector,
+                                    offsetspacings=offset_spacings)
+
+else:
+    print("No realms to plot")
 
 plt.axis("square")
 plt.show()
